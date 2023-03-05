5b839e03d3861b86c4cabe245fb35fa3c94a0699
fermer123 <fermer_525@mail.ru>
2023-03-05 18:26:49 +0500

    b4 fix cells

--- src/components/DatePick/PickDate.tsx
+++ src/components/DatePick/PickDate.tsx
@@ -1,4 +1,5 @@
 import {FC, useMemo, useState} from 'react';
+import style from './PickDate.module.scss';
 
 interface PickDateProps {
   value: Date;
@@ -20,18 +21,46 @@ const getDaysAmountInAMonth = (year: number, month: number) => {
 };
 
 const getPreviousMonthDays = (year: number, month: number) => {
+  const dateCells: DateCellItem[] = [];
   const currentMonthFirstDay = new Date(year, month, 1);
   const dayOfTheWeek = currentMonthFirstDay.getDay();
   const prevMonthCellsAmount = dayOfTheWeek - 1;
+  const daysAmountInPrevMonth = getDaysAmountInAMonth(year, month - 1);
+  const [cellYear, cellMonth] =
+    month === 0 ? [year - 1, 11] : [year, month - 1];
+  for (let i = 0; i < prevMonthCellsAmount; i += 1) {
+    dateCells.push({
+      year: cellYear,
+      month: cellMonth,
+      day: daysAmountInPrevMonth - i,
+    });
+  }
+  return dateCells;
 };
-const getNextMonthDays = () => {};
+const getNextMonthDays = (year: number, month: number) => {
+  const currentMonthFirstDay = new Date(year, month, 1);
+  const dayOfTheWeek = currentMonthFirstDay.getDay();
+  const prevMonthCellsAmount = dayOfTheWeek - 1;
+  const daysAmount = getDaysAmountInAMonth(year, month);
+  const nextMonthDays = 42 - daysAmount - prevMonthCellsAmount;
+  const dateCells: DateCellItem[] = [];
+  const [cellYear, cellMonth] = month === 0 ? [year + 1, 0] : [year, month + 1];
+  for (let i = 1; i < nextMonthDays; i += 1) {
+    dateCells.push({
+      year: cellYear,
+      month: cellMonth,
+      day: i,
+    });
+  }
+  return dateCells;
+};
 const getCurrentMonthDays = (
   year: number,
   month: number,
   numberOfDays: number,
 ) => {
   const dateCells: DateCellItem[] = [];
-  for (let i = 1; i < numberOfDays; i += 1) {
+  for (let i = 1; i <= numberOfDays; i += 1) {
     dateCells.push({
       year,
       month,
@@ -55,7 +84,14 @@ const PickDate: FC<PickDateProps> = ({value, onChange}) => {
   const dateCells = useMemo(() => {
     const items: DateCellItem[] = [];
     const daysInAMonth = getDaysAmountInAMonth(panelYear, panelMonth);
-    return items;
+    const currentMonthDays = getCurrentMonthDays(
+      panelYear,
+      panelMonth,
+      daysInAMonth,
+    );
+    const prevMonthDays = getPreviousMonthDays(panelYear, panelMonth);
+    const nextMonthDays = getNextMonthDays(panelYear, panelMonth);
+    return [...prevMonthDays, ...nextMonthDays, ...currentMonthDays];
   }, [panelYear, panelMonth]);
 
   const nextYear = () => {};
@@ -64,8 +100,15 @@ const PickDate: FC<PickDateProps> = ({value, onChange}) => {
   const prevMonth = () => {};
 
   return (
-    <div>
-      {day} {month} {year}
+    <div className={style.calendar}>
+      <div>
+        Date: {day} {month} {year}
+      </div>
+      <div className={style.cells}>
+        {dateCells.map((e) => (
+          <div>{e.day}</div>
+        ))}
+      </div>
     </div>
   );
 };
